e283b9fccd5bd0fc40202fe219dc8cb61ae5fdaa
Sasha <sanyokkotenko2002@gmail.com>
2019-09-11 13:21:43 +0300

    SecondProject

---
+++ ISMSecondConsoleProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ISMSecondConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Second Project");
+            Console.ReadKey();
+        }
+    }
+}
